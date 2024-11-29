29f9da9ca188ef830c6b239e4703dc58f8960d5f
BintangDanuarta <bintangdanuarta@outlook.com>
2024-11-30 06:18:51 +0700

    final commit

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,11 +1,12 @@
 const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
+const storeData = require('../services/storeData');
 
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
     const { model } = request.server.app;
 
-    const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+    const { label, suggestion } = await predictClassification(model, image);
     const id = crypto.randomUUID();
     const createdAt = new Date().toISOString();
 
@@ -13,13 +14,14 @@ async function postPredictHandler(request, h) {
         "id": id,
         "result": label,
         "suggestion": suggestion,
-        "confidenceScore": confidenceScore,
         "createdAt": createdAt
     }
 
+    await storeData(id, data);
+
     const response = h.response({
         status: 'success',
-        message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
+        message: 'Model is predicted successfully',
         data
     })
     response.code(201);
